62805c66919cbd797381a4cfc64b4a4092cdd0ab
Robert Lin <robert@bobheadxi.dev>
2021-01-11 14:32:54 +0800

    deno fmt

--- output.ts
+++ output.ts
@@ -1,11 +1,19 @@
 import { Question, Report, Standup, User } from "./geekbot.ts";
 
-export function renderResults(standup: Standup, user: User, question: Question, reports: Report[]) {
-    console.log(`${standup.channel} > ${user.username} > '${question.text}'`)
-    reports.forEach(r => {
-        if (r.questions.length > 0) {
-            console.log('%c' + new Date(r.timestamp * 1000).toLocaleString(), 'color:yellow;font-weight:bold')
-            console.log(r.questions.map(({ answer }) => answer).join(''))
-        }
-    })
+export function renderResults(
+  standup: Standup,
+  user: User,
+  question: Question,
+  reports: Report[],
+) {
+  console.log(`${standup.channel} > ${user.username} > '${question.text}'`);
+  reports.forEach((r) => {
+    if (r.questions.length > 0) {
+      console.log(
+        "%c" + new Date(r.timestamp * 1000).toLocaleString(),
+        "color:yellow;font-weight:bold",
+      );
+      console.log(r.questions.map(({ answer }) => answer).join(""));
+    }
+  });
 }
